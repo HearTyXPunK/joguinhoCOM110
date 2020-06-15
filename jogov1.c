453c87820e686796e77365f518e1e5e432075d8a
Maysuni <63670910+Maysuni@users.noreply.github.com>
2020-06-15 01:42:03 -0300

    Update and rename teste_jogo.c to jogov1.c

--- jogov1.c
+++ jogov1.c
@@ -6,18 +6,16 @@
 #include<locale.h>
 #include<windows.h>
 #include<conio.h>
-#define NL 2
-#define NC 2
 
 int main(){
-  	int i=1, j=1, op, op2,op3;
+  int i=1, j=1, op, op2,op3;
 	int k=0, t=0;
 	int A, B, C, D;
-	int numeros[NL][NC];
-	int resposta[NL][NC];
+	int numeros[2][2];
+	int resposta[2][2];
 	int acertos=0;
 	char c, nome[20];
-	int facil[2][2],i2, j2, par, pontos;
+	int facil[2][2],i2, j2, par, pontos=0;
   
 do{
 
@@ -29,16 +27,16 @@ do{
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c                                              1. Jogar                                                               %c\n", 179, 179);
     printf("%c                                              2. Sair                                                                %c\n", 179, 179);
-    printf("%c                                                                                                                     %c\n", 179, 179);
+    printf("%c                                              3. Créditos                                                            %c\n", 179, 179);
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 192, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 217);
-	 //atÃ© aqui Ã© o menu feito
-printf("\n  Op%c%co: ", 135, 198);
-scanf(" %d", &op);
-printf("\n");
+	 //até aqui menu geral feito
+		printf("\n  Op%c%co: ", 135, 198);
+		scanf(" %d", &op);
+		printf("\n");
 
 switch (op){
 	
@@ -58,11 +56,12 @@ switch (op){
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c                                                                                                                     %c\n", 179, 179);
     printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 192, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 217);
-	  //atÃ© aqui Ã© o menu 2 feito
+	  //até aqui menu de modo de jogo
 		printf("\n  Op%c%co: ", 135, 198);
 	  scanf(" %d", &op2);
 		switch (op2){
 			case 1:
+				//modo de jogo 1
 				system("cls");
 				printf("================================================ JOGO DA MEM%cRIA ======================================================\n", 224);
 		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 218, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 191);
@@ -77,127 +76,153 @@ switch (op){
 		    printf("%c                                                                                                                     %c\n", 179, 179);
 		    printf("%c                                                                                                                     %c\n", 179, 179);
 		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 192, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 217);
-			  //atÃ© aqui Ã© o menu 2 feito
+			  //até aqui menu de dificuldade
 				printf("\n  Op%c%co: ", 135, 198);
 			  scanf(" %d", &op3);
 			  switch(op3){
 			  	case 1:
-			  	setlocale(LC_ALL,"");
-					srand(time(NULL));
-					printf("%40c Jogador, digite seu nome: ", ' ');
-					scanf("%s", nome);
-					i=0;
-					j=0;
-					for(k=0; k<5; k++){
-					printf("\nJogada %d: \n", k+1);
-					printf("Memorize os números abaixo: \n");
-					printf("PRESSIONE ENTER PARA INICIAR.\n");
-					do{
-			        	c = getch();
-			    	}
-					while (c != 13);
-					
-					for(i=0; i<NL; i++){
-						for(j=0; j<NC; j++){
-							numeros[i][j] = (rand() % 30);
+				  	//jogo no fácil
+						srand(time(NULL));
+						printf("%40c Jogador, digite seu nome: ", ' ');
+						scanf("%s", nome);
+						i=0;
+						j=0;
+						for(k=0; k<5; k++){
+						printf("\n%40c Jogada %d: \n", ' ', k+1);
+						printf("%40c Memorize os numeros! \n",' ');
+						printf("%40c PRESSIONE ENTER PARA INICIAR.\n",' ');
+						do{
+				        	c = getch();
+				    	}
+						while (c != 13);
+						
+						for(i=0; i<2; i++){
+							for(j=0; j<2; j++){
+								numeros[i][j] = (rand() % 30);
+								}
+						}
+						
+						t=5;
+						do{
+							system("cls");
+							printf("%50c Tempo: %d\n\n", ' ', t);
+							t--;
+							
+							for(i=0; i<2; i++){
+								printf("%51c ", ' ');
+								for(j=0; j<2; j++){
+									printf("%d ", numeros[i][j]);
+								}
+							printf("\n");	
 							}
-					}
-					
-					t=5;
-					do{
+							sleep(1);
+						}
+						while(t>0);
 						system("cls");
-						printf("Tempo: %d\n\n", t);
-						t--;
-						for(i=0; i<NL; i++){
-							printf("   ");
-							for(j=0; j<NC; j++){
-								printf("%d ", numeros[i][j]);
-							}
-						printf("\n");	
-						}
+						printf("\n%30cInsira os numeros memorizados de acordo com a matriz: \n",' ');
+						printf("\n%50c  |A  B|\n%50c  |C  D|\n\n",' ',' ');
+						printf("%50c  A = ",' ');
+						scanf("%d", &resposta[0][0]);
+						printf("%50c  B = ",' ');
+						scanf("%d", &resposta[0][1]);
+						printf("%50c  C = ",' ');
+						scanf("%d", &resposta[1][0]);
+						printf("%50c  D = ",' ');
+						scanf("%d", &resposta[1][1]);
+						printf("\n");
 						sleep(1);
-					}
-					while(t>0);
-					system("cls");
-					printf("\nInsira os numeros memorizados de acordo com a matriz: \n");
-					printf("\n  |A  B|\n  |C  D|\n\n");
-					printf("  A = ");
-					scanf("%d", &resposta[0][0]);
-					printf("  B = ");
-					scanf("%d", &resposta[0][1]);
-					printf("  C = ");
-					scanf("%d", &resposta[1][0]);
-					printf("  D = ");
-					scanf("%d", &resposta[1][1]);
-					printf("\n");
-					sleep(1);
-					system("cls");
-					for(i=0; i<NL; i++){
-						for(j=0; j<NC; j++){
-							if(resposta[i][j] == numeros[i][j]){
-								acertos++;
-							}
-							else{
-								break;
-							}
-						}
-					}
-					}
-					printf("Cada acerto vale um ponto. \n");
-					printf("%s, TOTAL DE PONTOS = %d de 24\n\n", nome, acertos);
-					system("pause");
-					system("cls");
-					break;
-			case 2:
-				system("cls");
-				printf("================================================ JOGO DA MEM%cRIA ======================================================\n", 224);
-		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 218, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 191);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c                                               Escolha a dificuldade                                                 %c\n", 179, 179);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c                                                1.Fácil                                                              %c\n", 179, 179);
-		    printf("%c                                                2.Médio                                                              %c\n", 179, 179);
-		    printf("%c                                                3.Difícil                                                            %c\n", 179, 179);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c                                                                                                                     %c\n", 179, 179);
-		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 192, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 217);
-			  //atÃ© aqui Ã© o menu 2 feito
-				printf("\n  Op%c%co: ", 135, 198);
-			  scanf(" %d", &op3);
-			  switch(op3){
-			  	case 1:
-							srand(time(NULL));
-								for(i=0;i<2;i++){
-									for(j=0;j<2;j++){
-										if(i==1 && j==1)
-											facil[i][j]= facil[0][0];
-										else
-											facil[i][j]= rand() % 100;
+						system("cls");
+						
+						for(i=0; i<2; i++){
+							for(j=0; j<2; j++){
+								if(resposta[i][j] == numeros[i][j]){
+									acertos++;
+								}
+								else{
+									break;
 									}
 								}
+							}
+						}
+						printf("Cada acerto vale um ponto. \n");
+						printf("%s, TOTAL DE PONTOS = %d de 24\n\n", nome, acertos);
+						system("pause");
+						system("cls");
+						break;
+					//case 2:
+					//médio
+			}
+			case 2:
+				//modo de jogo 2
+				system("cls");
+				printf("================================================ JOGO DA MEM%cRIA ======================================================\n", 224);
+		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 218, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 191);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c                                               Escolha a dificuldade                                                 %c\n", 179, 179);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c                                                1.Fácil                                                              %c\n", 179, 179);
+		    printf("%c                                                2.Médio                                                              %c\n", 179, 179);
+		    printf("%c                                                3.Difícil                                                            %c\n", 179, 179);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c                                                                                                                     %c\n", 179, 179);
+		    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%C%c%c%c%c%c%c\n", 192, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 217);
+			  //até aqui menu de dificuldade
+				printf("\n  Op%c%co: ", 135, 198);
+			  scanf(" %d", &op3);
+			  switch(op3){
+			  	case 1:
+			  			//fácil
+			  			printf("%40c Jogador, digite seu nome: ", ' ');
+							scanf("%s", nome);
+							
+			  			system("cls");
+			  			srand(time(NULL));
 							for(i=0;i<2;i++){
 								for(j=0;j<2;j++){
-									printf(" %d", facil[i][j]);
+									if(i==1 && j==1)
+										facil[i][j]= facil[0][0];
+									else
+										facil[i][j]= rand() % 100;
 								}
-								printf("\n");
 							}
-							printf("Digite o número que tem um par:");
+							t=2;
+							do{
+								system("cls");
+								printf("%50c Tempo: %d\n\n", ' ', t);
+								t--;
+								
+								for(i=0;i<2;i++){
+									printf("\n%51c",' ');
+									for(j=0;j<2;j++){
+										printf("%d ", facil[i][j]);
+									}
+								}
+								sleep(1);
+							}
+							while(t>0);
+							system("cls");
+							printf("%40c Digite o número que tem um par:",' ');
 							scanf(" %d", &par);
 							if(par == facil[0][0]){
-								printf("\nParabéns você acertou!");
+								printf("\n%40c Parabéns você acertou!\n",' ');
 								pontos++;
+								printf("%30c Você tem %d ponto(s).",' ', pontos);
 							}
-							else
-								printf("\nVocê errou!");
+							else{
+								printf("\n%40c Você errou!\n",' ');
+								printf("%30c Você tem %d ponto(s).",' ', pontos);
+							}
+							system("pause");
+							system("cls");
 							break;
-						}	
-			break;
-			}
-		}
-	case 2: 
-	
+					//case 2:
+						//médio
+				}
+	}
+	case 2:
+	//sai do jogo no loop
 	break;
 
 	}
